0e108d7eb2cea75997341413a8a59257b87bf2a7
identity-fraud <william.kim22@education.nsw.gov.au>
2026-03-30 13:30:42 +1100

    fix button

--- site/script.js
+++ site/script.js
@@ -1,4 +1,13 @@
 function dentritic() {
-    document.getElementById("dendritic").style.display="inline";
+    var dendritic_id = document.getElementById("dendritic")
+    var dendritic_css = window.getComputedStyle(dendritic_id)
+    var display_check = dendritic_css.getPropertyValue("display");
+
+    if (display_check === "none") {
+        dendritic_id.style.display="inline";
+    } else {
+        dendritic_id.style.display="none";
+    }
+
     console.log("dendritic success");
 }
