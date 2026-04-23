6e705bb0ad8fd888870351573bbad316ffac9db2
transaction-fraud <william.kim22@education.nsw.gov.au>
2026-04-23 22:32:06 +1000

    hover and zoom when clicked images

--- site/script.js
+++ site/script.js
@@ -1,13 +1,25 @@
-function dentritic() {
-    var dendritic_id = document.getElementById("dendritic")
-    var dendritic_css = window.getComputedStyle(dendritic_id)
-    var display_check = dendritic_css.getPropertyValue("display");
-
-    if (display_check === "none") {
-        dendritic_id.style.display="inline";
-    } else {
-        dendritic_id.style.display="none";
+function zoom(el) {
+    if (el.classList.contains('is-fullscreen')) {
+        el.style.transform = '';
+        el.classList.remove('is-fullscreen');
+        return;
+    }
+    const activeImage = document.querySelector('.is-fullscreen');
+    if (activeImage) {
+        return;
     }
 
-    console.log("dendritic success");
+    const rect = el.getBoundingClientRect();
+    
+    const screenCenterX = window.innerWidth / 4;
+    const screenCenterY = window.innerHeight / 2;
+
+    const elementCenterX = rect.left + rect.width / 2;
+    const elementCenterY = rect.top + rect.height / 2;
+
+    const xMove = screenCenterX - elementCenterX;
+    const yMove = screenCenterY - elementCenterY;
+
+    el.style.transform = `translate(${xMove}px, ${yMove}px) scale(3)`;
+    el.classList.add('is-fullscreen');
 }
