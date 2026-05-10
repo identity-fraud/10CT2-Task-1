9384be2a54d20a8742b12702ae7828fdef6d4571
transaction-fraud <william.kim22@education.nsw.gov.au>
2026-05-10 12:59:28 +1000

    fix dendritic spelling and add sidebar

--- site/script.js
+++ site/script.js
@@ -1,7 +1,12 @@
 function zoom(el) {
+    const sidebar = document.querySelector('.sidebar');
+    const sidebarTitle = document.getElementById('sidebar-title')
+    const sidebarDescription = document.getElementById('sidebar-description')
     if (el.classList.contains('is-fullscreen')) {
         el.style.transform = '';
         el.classList.remove('is-fullscreen');
+        sidebarTitle.innerText = '';
+        sidebarDescription.innerText = '';
         return;
     }
     const activeImage = document.querySelector('.is-fullscreen');
@@ -22,4 +27,21 @@ function zoom(el) {
 
     el.style.transform = `translate(${xMove}px, ${yMove}px) scale(3)`;
     el.classList.add('is-fullscreen');
+    const exactCellname = el.alt.replace(' cell vector drawing', '')
+    sidebarTitle.innerText = exactCellname + ' ' + "cell";
+    sidebarDescription.innerText = getCellDescription(exactCellname);
+}
+
+function getCellDescription(name) {
+    const descriptions = {
+        'dendritic': "Dendritic cells are messengers that present antigens to T cells.",
+        'neutrophil': "Neutrophils are the first responders to infection, eating pathogens.",
+        'macrophage': "Macrophages are 'big eaters' that clean up debris and dead cells.",
+        'natural killer': "NK cells destroy virally infected cells and tumor cells.",
+        'mast': "Mast cells release histamine during allergic reactions.",
+        'T': "T cells are the 'generals' that coordinate the adaptive response.",
+        'B': "B cells produce antibodies to neutralize specific threats."
+    };
+
+    return descriptions[name]
 }
